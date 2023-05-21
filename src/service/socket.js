8738cac9695d01fdd9ffe977a0430f886bc435e1
isaac <isaaconyes80@gmail.com>
2023-05-21 22:59:23 +0100

    remake

--- src/service/socket.js
+++
@@ -1,5 +0,0 @@
-import io from 'socket.io-client'
-export const ENDPOINT = 'https://linkedin-clone-api.onrender.com'
-// export const ENDPOINT = 'http://localhost:4000'
-
-export const socket = io(ENDPOINT)
--- src/utils/axiosFetch.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-
-axios.defaults.withCredentials = true
-
-export const axiosFetch = axios.create({
-    baseURL: 'https://linkedin-clone-api.onrender.com/api/',
-    // baseURL: 'http://localhost:4000/api',
-})
